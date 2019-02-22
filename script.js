ebb56688348a90d6662a8a00d43b50977eb81ba8
Glitch (nyc-film-permits) <none>
2019-02-22 02:27:29 +0000

    💅💧 Updated with Glitch

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-// let the editor know that `Chart` is defined by some code
-// included in another file (in this case, `index.html`)
-// Note: the code will still work without this line, but without it you
-// will see an error in the editor
+// Global variables defined outside of this script file to avoid the code editor reporting undefined variables.
 /* global Chart */
 
-// create data objects
+const colorPalette = [
+  /* from colorbrewer2.org */
+  'rgb(179,0,0)',
+  'rgb(227,74,51)',
+  'rgb(252,141,89)',
+  'rgb(253,204,138)',
+  'rgb(254,240,217)'
+];
+
+function colorOpacity( color, opacity ){
+  /* A helper function that takes an rgb color and adds opacity to it. */
+  opacity = opacity || 0.5 /* If no value is passed to the function, 0.5 will be used as a default value. */;
+  return color.replace('rgb', 'rgba').replace(')', ',' + opacity + ')');
+}
+
+/* Objects holding the data. */
+
 const permitsByBorough = [
   {
     "borough": "Manhattan",
@@ -86,99 +99,323 @@ const permitsByType = [
   }
 ];
 
-// create an array of continents to use as labels for the charts
-const boroughs = permitsByBorough.map( function( dataPoint ){
+const permitsByCountry = [
+  {
+    "country": "USA 🇺🇸",
+    "permits": 40125
+  },
+  {
+    "country": "UK 🇬🇧",
+    "permits": 10
+  },
+  {
+    "country": "Japan 🇯🇵",
+    "permits": 8
+  },
+  {
+    "country": "France 🇫🇷",
+    "permits": 7
+  },
+  {
+    "country": "Panama 🇵🇦",
+    "permits": 7
+  },
+  {
+    "country": "Canada 🇨🇦",
+    "permits": 6
+  },
+  {
+    "country": "Australia 🇦🇺",
+    "permits": 5
+  },
+  {
+    "country": "Netherlands 🇳🇱",
+    "permits": 3
+  },
+  {
+    "country": "Germany 🇩🇪",
+    "permits": 1
+  }
+];
+
+/* Extracting data and labels. */
+
+const boroughLabels = permitsByBorough.map(  function(  dataPoint  ){
   return dataPoint.borough;
 } );
 
-const permits = permitsByBorough.map( function( dataPoint ){
+const permitsByBoroughData = permitsByBorough.map(  function(  dataPoint  ){
   return dataPoint.permits;
-} );
+}  );
 
-const bar = document.getElementById('permits-by-borough-bar-chart').getContext('2d');
-const barChart = new Chart(bar, {
-  type: 'bar', // make it a bar chart
+const permitsByYearLabels = permitsByYear.map(  function(  dataPoint  ){
+  return dataPoint.year;
+}  );
+
+const permitsByYearData = permitsByYear.map(  function(  dataPoint  ){
+  return dataPoint.permits;
+}  );
+
+const permitsByTypeLabels = permitsByType.map(  function(  dataPoint  ){
+  return dataPoint.type;
+}  );
+
+const permitsByTypeData = permitsByType.map(  function(  dataPoint  ){
+  return dataPoint.permits;
+}  );
+
+const permitsByCountryLabels = permitsByCountry.map(  function(  dataPoint  ){
+  return dataPoint.country;
+}  );
+
+const permitsByCountryData = permitsByCountry.map(  function(  dataPoint  ){
+  return dataPoint.permits;
+}  );
+
+/* Inserting charts. */
+
+const permitsByBoroughBar = document.getElementById( 'permits-by-borough-bar-chart' ).getContext( '2d' );
+const permitsByBoroughBarChart = new Chart( permitsByBoroughBar, {
+  type: 'bar',
   data: {
-    labels: boroughs,
+    labels: boroughLabels,
     datasets: [{
       label: 'Permits issued',
-      data: permits, // use the array of permits to draw bars
-      backgroundColor: 'rgba(255, 99, 132, 0.2)', // make the bars translucent red
-      borderColor: 'rgba(255, 99, 132, 1)', // make the borders of the bars opaque red
-      borderWidth: 1 // set the border width to 1 pixel
+      data: permitsByBoroughData,
+      backgroundColor: colorOpacity( colorPalette[1], 0.5 ),
+      borderColor: colorPalette[1],
+      borderWidth: 1
     }]
   },
   options: {
-    // the y-axis should start at 0
     scales: {
       yAxes: [{
         ticks: {
           beginAtZero:true,
-          userCallback: function(value, index, values) {
+          userCallback: function( value, index, values ) {
               value = value.toString();
-              value = value.split(/(?=(?:...)*$)/);
-              value = value.join(',');
+              value = value.split( /(?=(?:...)*$)/ );
+              value = value.join( ',' );
               return value;
           }
-          
         }
       }]
     },
     tooltips: {
       callbacks: {
-        label: function(tooltipItem, data) {
-          var value = permitsByBorough.datasets[0].data[tooltipItem.index];
+        label: function( tooltipItem, data ) {
+          var value = permitsByBorough[tooltipItem.index].permits;
           value = value.toString();
-          value = value.split(/(?=(?:...)*$)/);
-          value = value.join(',');
-          return value;
+          value = value.split( /(?=(?:...)*$)/ );
+          value = value.join( ',' );
+          return value + ' permits';
         }
       }
     }
   }
-});
+} );
 
-// initialize a chart and put it in the 'permits-by-borough-pie-chart' div
-const permitsByBoroughPie = document.getElementById('permits-by-borough-pie-chart').getContext('2d');
-const permitsByBoroughPiehart = new Chart(permitsByBoroughPie, {
-  type: 'pie', // make it a pie chart
+const permitsByBoroughPie = document.getElementById( 'permits-by-borough-pie-chart' ).getContext( '2d' );
+const permitsByBoroughPieChart = new Chart( permitsByBoroughPie, {
+  type: 'pie',
   data: {
-    labels: boroughs, // use the array of boroughs to label each 
+    labels: boroughLabels,
     datasets: [{
-      data: permits, // use the array of boroughs to draw pie slices
-      // set each pie slice to a translucent color
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      // set the border of each pie slice to the same color as the background
-      // of the slice but opaque
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1 // set border width to 1 pixel
+      data: permitsByBoroughData,
+      backgroundColor: colorPalette.map( function( color ){
+        return colorOpacity( color );
+      } ),
+      borderColor: colorPalette[1],
+      borderWidth: 1
     }]
   },
   options: {
     tooltips: {
       callbacks: {
-        label: function(tooltipItem, data) {
+        label: function( tooltipItem, data ) {
           var value = permitsByBorough[tooltipItem.index].permits;
           value = value.toString();
-          value = value.split(/(?=(?:...)*$)/);
-          value = value.join(',');
-          return value;
+          value = value.split( /(?=(?:...)*$)/ );
+          value = value.join( ',' );
+          return permitsByBorough[tooltipItem.index].borough +  ': ' + value + ' permits';
         }
       }
     }
   }  
-});
+} );
+
+const permitsByYearElement = document.getElementById( 'permits-by-year' ).getContext( '2d' );
+const permitsByYearChart = new Chart( permitsByYearElement, {
+  type: 'line',
+  data: {
+    labels: permitsByYearLabels,
+    datasets: [{
+      label: 'Permits issued',      
+      data: permitsByYearData,
+      backgroundColor: colorOpacity( colorPalette[1] ),
+      borderColor: colorPalette[1],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true,
+          userCallback: function( value, index, values ) {
+              value = value.toString();
+              value = value.split( /(?=(?:...)*$)/ );
+              value = value.join( ',' );
+              return value;
+          }
+          
+        }
+      }]
+    },    
+    tooltips: {
+      callbacks: {
+        label: function( tooltipItem, data ) {
+          var value = permitsByYear[tooltipItem.index].permits;
+          value = value.toString();
+          value = value.split( /(?=(?:...)*$)/ );
+          value = value.join( ',' );
+          return value + ' permits';
+        }
+      }
+    }
+  }  
+} );
+
+const permitsByTypeElement = document.getElementById( 'permits-by-type' ).getContext( '2d' );
+const permitsByTypeChart = new Chart( permitsByTypeElement, {
+  type: 'bar',
+  data: {
+    labels: permitsByTypeLabels,
+    datasets: [{
+      label: 'Permits issued',      
+      data: permitsByTypeData,
+      backgroundColor: colorOpacity( colorPalette[1] ),
+      borderColor: colorPalette[1],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      xAxes: [{
+          type: 'category',
+          ticks: {
+            autoSkip: false,
+          }
+      }],      
+      yAxes: [{
+        type: 'logarithmic',
+        ticks: {
+          stepSize: 10000,          
+          beginAtZero:true,
+          userCallback: function( value, index, values ) {
+              value = value.toString();
+              value = value.split( /(?=(?:...)*$)/ );
+              value = value.join( ',' );
+              return value;
+          }          
+        },
+        afterBuildTicks: function( chart ) {    
+            chart.ticks = [];
+            chart.ticks.push( 100 );
+            chart.ticks.push( 200 );
+            chart.ticks.push( 500 );
+            chart.ticks.push( 2000 );
+            chart.ticks.push( 5000 );
+            chart.ticks.push( 10000 );
+          }
+        
+      }]
+    },    
+    tooltips: {
+      callbacks: {
+        label: function( tooltipItem, data ) {
+          var value = permitsByType[tooltipItem.index].permits;
+          value = value.toString();
+          value = value.split( /(?=(?:...)*$)/ );
+          value = value.join( ',' );
+          return value + ' permits';
+        }
+      }
+    }
+  }  
+} );
+
+const permitsByCountryElement = document.getElementById( 'permits-by-country' ).getContext( '2d' );
+const permitsByCountryChart = new Chart( permitsByCountryElement, {
+  type: 'bar',
+  data: {
+    labels: permitsByCountryLabels,
+    datasets: [{
+      label: 'Permits issued',      
+      data: permitsByCountryData,
+      backgroundColor: colorOpacity( colorPalette[1] ),
+      borderColor: colorPalette[1],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      xAxes: [{
+          type: 'category',
+          ticks: {
+            autoSkip: false,
+          }
+      }],      
+      yAxes: [{
+        type: 'logarithmic',
+        ticks: {
+          stepSize: 10000,          
+          beginAtZero:true,
+          userCallback: function( value, index, values ) {
+              value = value.toString();
+              value = value.split( /(?=(?:...)*$)/ );
+              value = value.join( ',' );
+              return value;
+          }          
+        },
+        afterBuildTicks: function( chart ) {    
+            chart.ticks = [];
+            chart.ticks.push( 1 );
+            chart.ticks.push( 10 );
+            chart.ticks.push( 1000 );
+            chart.ticks.push( 40000 );
+          }
+      }]
+    },    
+    tooltips: {
+      callbacks: {
+        label: function( tooltipItem, data ) {
+          var value = permitsByCountry[tooltipItem.index].permits;
+          value = value.toString();
+          value = value.split( /(?=(?:...)*$)/ );
+          value = value.join( ',' );
+          return value + ' permits';
+        }
+      }
+    }
+  }  
+} );
+
+/* Clicking on a section title scrolls the section into view and updates the URL linking directly to the section */
+
+const sectionHeaders = document.getElementsByTagName(  'h2'  );
+
+for (  let sectionHeaderIndex = 0; sectionHeaderIndex < sectionHeaders.length; sectionHeaderIndex++  ){
+  sectionHeaders[sectionHeaderIndex].onclick = function(  ev  ){
+    try{
+      ev.target.scrollIntoView( {
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'start'        
+      } );
+      if (window.history.replaceState) {
+         window.history.replaceState( null, null, '#' + ev.target.id );
+      }
+    } catch( err ){ /* noop */ }
+  }
+}
